c4774d97e08509440250629937169b40560dd6d3
Richard Berendsen <guust@richardberendsen.nl>
2024-05-06 17:25:38 +0200

    WIP capturing Snowflake objects

--- src/snowflake.go
+++ src/snowflake.go
@@ -13,7 +13,7 @@ import (
 	_ "github.com/snowflakedb/gosnowflake"
 )
 
-var validUnquotedExpr *regexp.Regexp = regexp.MustCompile(`^[a-z_*][a-z0-9_$*]{0,254}$`) // lowercase identifier chars + wildcard expansion
+var validUnquotedExpr *regexp.Regexp = regexp.MustCompile(`^[a-z_][a-z0-9_$]{0,254}\*?$`) // lowercase identifier chars + optional wildcard suffix
 var validQuotedExpr *regexp.Regexp = regexp.MustCompile(`.{0,255}`)
 
 
@@ -32,19 +32,6 @@ var dbTypeCast = map[string]dbType{
 	"TABLE": table,
 	"VIEW":  view,
 }
-type node struct {
-	chld map[dbObj]*node
-}
-type dbObj struct {
-	db string
-	schema string
-	name string
-	dbType dbType
-	// by the time we start supporting stored procedures we'd have to come up with
-	// a comparabe data structure that captures a variable amount of argument types
-	// cause Snowflake supports stored procedure overloading; probably just add a string
-	// with types separated by semicolons or something
-}
 
 
 // caching objects in Snowflake locally
@@ -62,6 +49,9 @@ type dbCache struct {
 type schemaCache struct {
 	dbName string
 	schemaName string
+	// note that if during run time a table is removed, and a view is
+	// created with the same name or vice versa then tableNames and
+	// viewNames can contain duplicate keys wrt each other
 	tableNames map[string]bool
 	viewNames map[string]bool
 }
@@ -71,14 +61,12 @@ func (c *accountCache) addDBs() {
 	if err != nil {
 		log.Fatalf("querying snowflake: %s", err)
 	}
-	c.dbs = make(map[string]*dbCache)
-	c.dbNames = make(map[string]bool)
 	for rows.Next() {
 		var dbName string
 		if err = rows.Scan(&dbName); err != nil {
 			log.Fatalf("error scanning row: %s", err)
 		}
-		c.dbs[dbName] = &dbCache{nil}
+		c.dbs[dbName] = &dbCache{dbName, map[string]*schemaCache{}, map[string]bool{}}
 		c.dbNames[dbName] = true
 	}
 	if err = rows.Err(); err != nil {
@@ -105,14 +93,12 @@ func (c *dbCache) addSchemas() {
 	if err != nil {
 		log.Fatalf("querying snowflake: %s", err)
 	}
-	c.schemas = make(map[string]*schemaCache)
-	c.schemaNames = make(map[string]bool)
 	for rows.Next() {
 		var schemaName string
 		if err = rows.Scan(&schemaName); err != nil {
 			log.Fatalf("error scanning row: %s", err)
 		}
-		c.schemas[schemaName] = &schemaCache{nil}
+		c.schemas[schemaName] = &schemaCache{c.dbName, schemaName, map[string]bool{}, map[string]bool{}}
 		c.schemaNames[schemaName] = true
 	}
 	if err = rows.Err(); err != nil {
@@ -140,7 +126,6 @@ func (c *schemaCache) addTables() {
 	if err != nil {
 		log.Fatalf("querying snowflake: %s", err)
 	}
-	c.tableNames = make(map[string]bool)
 	for rows.Next() {
 		var tableName string
 		if err = rows.Scan(&tableName); err != nil {
@@ -166,7 +151,6 @@ func (c *schemaCache) addViews() {
 	if err != nil {
 		log.Fatalf("querying snowflake: %s", err)
 	}
-	c.viewNames = make(map[string]bool)
 	for rows.Next() {
 		var viewName string
 		if err = rows.Scan(&viewName); err != nil {
@@ -196,19 +180,37 @@ type dbObjs struct {
 }
 
 type schemaObjs struct {
+	// note that in case of drift during runtime tables and views may
+	// contain the same keys (i.e., if during runtime a table was removed
+	// and a view with the same name created)
 	tables map[string]bool
 	views map[string]bool
 }
 
+func (o accountObjs) addObject(db string, schema string, obj string, t dbType) accountObjs {
+        dbObjs, ok := o.dbs[db]
+	if !ok {
+		o.dbs[db] = dbObjs{map[string]schemaObjs{}}
+	}
+	schemaObjs, ok := o.dbs[db].schemas[schema]
+	if !ok {
+		o.dbs[db].schemas[schema] = schemaObjs{map[string]bool{}, map[string]bool{}}
+	}
+	if t == table {
+		o.dbs[db].schemas[schema].tables[obj] = true
+	} else if t == view {
+		o.dbs[db].schemas[schema].views[obj] = true
+	}
+	return o
+}
+
 func (lhs accountObjs) subtract(rhs accountObjs) accountObjs {
-	res = accountObjs{make(map[string]dbObjs)}
+	res = accountObjs{map[string]dbObjs{}}
 	for k, v := range lhs.dbs {
-		v2, ok := rhs.dbs[k]
-		if !ok {
+		if v2, ok := rhs.dbs[k]; !ok {
 			res.dbs[k] = v
 		} else {
-			dbObjs := v.subtract(v2)
-			if len(dbObjs) > 0 {
+			if dbObjs := v.subtract(v2); len(dbObjs) > 0 {
 				res.dbs[k] = dbObjs
 			}
 		}
@@ -217,14 +219,12 @@ func (lhs accountObjs) subtract(rhs accountObjs) accountObjs {
 }
 
 func (lhs dbObjs) subtract(rhs dbObjs) dbObjs {
-	res = dbObjs{make(map[string]schemaObjs)}
+	res = dbObjs{map[string]schemaObjs{}}
 	for k, v := range lhs.schemas {
-		v2, ok := rhs.schemas[k]
-		if !ok {
+		if v2, ok := rhs.schemas[k]; !ok {
 			res.schemas[k] = v
 		} else {
-			schemaObjs := v.subtract(v2)
-			if len(schemaObjs) > 0 {
+			if schemaObjs := v.subtract(v2); len(schemaObjs) > 0 {
 				res.schemas[k] = schemaObjs
 			}
 		}
@@ -233,21 +233,42 @@ func (lhs dbObjs) subtract(rhs dbObjs) dbObjs {
 }
 
 func (lhs schemaObjs) subtract(rhs schemaObjs) schemaObjs {
-	res = schemaObjs{make(map[string]bool), make(map[string]bool)}
-	for k, v := range lhs.tables {
-		v2, ok := rhs.tables[k]
-		if !ok {
-			res.tables[k] = v
+	res = schemaObjs{map[string]bool{}, map[string]bool{}}
+	for k, _ := range lhs.tables {
+		if _, ok := rhs.tables[k]; !ok {
+			res.tables[k] = true
 		}
 	}
-	for k, v := range lhs.views {
-		v2, ok := rhs.views[k]
-		if !ok {
-			res.views[k] = v
+	for k, _ := range lhs.views {
+		if _, ok := rhs.views[k]; !ok {
+			res.views[k] = true
 		}
 	}
 	return res
 } 
+
+func (lhs accountObjs) add(rhs accountObjs) accountObjs {
+	for k, v := range rhs.dbs {
+		if v2, ok := lhs.dbs[k]; !ok { lhs.dbs[k] = v } else { lhs.dbs[k] = lhs.dbs[k].add(rhs.dbs[k]) }
+	}
+	return lhs
+}
+
+func (lhs dbObjs) add(rhs dbObjs) dbObjs {
+	for k, v := range rhs.schemas {
+		if v2, ok := lhs.schemas[k]; !ok { lhs.schemas[k] = v } else { lhs.schemas[k] = lhs.schemas[k].add(rhs.schemas[k]) }
+	}
+	return lhs
+}
+
+func (lhs schemaObjs) add(rhs schemaObjs) schemaObjs {
+	for k, _ := range rhs.tables {
+		if _, ok := lhs.tables[k]; !ok { lhs.tables[k] = true }
+	}
+	for k, _ := range rhs.views {
+		if _, ok := lhs.views[k]; !ok { lhs.views[k] = true }
+	}
+}
 
 func (old accountObjs) actions(new accountObjs) actions {
 }
@@ -342,32 +363,43 @@ func escapeString(s string) string {
 	return strings.ReplaceAll(s, "'", "\\'")
 }
 
+func escapeRegExp(s string) string {
+	return strings.ReplaceAll(s, "$", "\$")
+}
+
 func matchPart(e exprPart, l map[string]bool) map[string]bool {
 	r := map[string]bool{}
 	if e.is_quoted {
-		_, ok := l[e.s]
-		if ok {
+		if _, ok := l[e.s]; ok {
 			r[e.s] = true
 		}
 		return r
 	} 
-	// implement match unquoted with wildcard
+	// implement match unquoted with optional suffix wildcard
+	// note that we match case insensitive, so `mytable` would match all of 
+	// "mytable", "MyTable", "MYTABLE", etc.
+	var validMatchingExpression *regexp.Regexp = regexp.MustCompile("(?i)" + e.s)
+	for k, _ := range l {
+		if validMatchingExpression.MatchString(l) {
+			r[k] = true
+		}
+	}
 	return r
 }
 
-func match(e expr, c *accountCache, o *accountObjs) map[dbObj]bool {
+func match(e expr, c *accountCache) {
+	o := accountObjs{map[string]dbObjs{})
 	matchedDBs := matchPart(e[database], c.getDBnames())
 	for db, _ := range matchedDBs {
 		matchedSchemas := matchPart(e[schema], c.getDBnames()[db].getSchemaNames())
 		for schema, _ := range matchedSchemas {
 			matchedTables := matchPart(e[table], c.getDBnames()[db].getSchemaNames()[schema].getTableNames())	
 			matchedViews := matchPart(e[table], c.getDBnames()[db].getSchemaNames()[schema].getViewNames())	
-			for t, _ := range matchedTables {
-				o.dbs[db].schemas[schema].tables[t] = true // does not quite work yet cause need to initialize stuff
-			}	
+			for t, _ := range matchedTables { o = o.addObject(db, schema, t, table) }	
+			for v, _ := range matchedViews { o = o.addObject(db, schema, v, view) }	
 		}
 	}
-	return m
+	return o
 }
 
 func querySnowflake(g *grupsDiff) {
@@ -378,20 +410,17 @@ func querySnowflake(g *grupsDiff) {
 	// for deleted products we don't need to know the objects for now
 
 	// as we match databases and schema's, we build up a local cache of the DB tree.
-	c := &accountCache{nil, nil}
-	_ = c.getDBnames()
+	c := &accountCache{map[string]*dbCache{}, map[string]bool{}}
 	for _, p := range g.created {
+		p.matchedInclude = accountObjs{map[string]dbObjs{}}
 		for e, _ := range p.exprs {
-			for o, _ :=  range match(e, c, p) {
-				p.matched[o] = true
-			}
+			p.matchedInclude = p.matchedInclude.add(match(e, c))
 		}
-		p.matchedExclude = make(map[dbObj]bool)
+		p.matchedExclude = accountObjs{map[string]dbObjs{}}
 		for e, _ := range p.exprsExclude {
-			for o, _ :=  range match(e, &accountNode) {
-				p.matchedExclude[o] = true
-			}
+			p.matchedExclude = p.matchedExclude.add(match(e, c))
 		}
+		p.matched = p.matchedInclude.subtract(p.matchedExclude)
 	}
 }
 
