b60a435efaa233a091fecbd53fbefa63a4f0214f
Richard Berendsen <guust@richardberendsen.nl>
2024-09-17 13:29:10 +0200

    WIP input validation

--- internal/syntax/elmntor.go
+++ internal/syntax/elmntor.go
@@ -9,8 +9,37 @@ import (
 
 
 type ElmntOr struct {
-	ProducingService ProducingService `yaml:"producing_service,omitempty"`
-	Product Product `yaml:",omitempty"`
-	Interface Interface`yaml:"interface,omitempty"`
+	ProducingService *ProducingService `yaml:"producing_service,omitempty"`
+	Product *Product `yaml:",omitempty"`
+	Interface *Interface`yaml:"interface,omitempty"`
+}
+
+func (e ElmntOr) validate() error {
+	n_elements := 0
+	if e.ProducingService != nil {
+		n_elements += 1
+		err := e.ProducingService.validate()
+		if err != nil {
+			return fmt.Errorf("validating ProducingService: %v", err)
+		}
+	}
+	if e.Product != nil {
+		n_elements += 1
+		err := e.Product.validate()
+		if err != nil {
+			return fmt.Errorf("validating Product: %v", err)
+		}
+	}
+	if e.Interface != nil {
+		n_elements += 1
+		err := e.Interface.validate()
+		if err != nil {
+			return fmt.Errorf("validating Interface: %v", err)
+		}
+	}
+	if n_elements != 1 {
+		return fmt.Errorf("not exactly one element in ElmntOr")
+	}
+	return nil
 }
 
--- internal/syntax/grupin.go
+++ internal/syntax/grupin.go
@@ -3,6 +3,7 @@ package syntax
 import (
 	"io"
 	"fmt"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -14,24 +15,28 @@ func NewGrupin(r io.Reader) (*Grupin, error) {
 	dec.KnownFields(true)
 	var g Grupin
 	for {
-		var elmnt ElmntOr;
-		err := dec.Decode(&elmnt)
+		var e ElmntOr;
+		err := dec.Decode(&e)
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
 			return r, fmt.Errrorf("decoding YAML: %s", err)
 		}
-		g = append(g, elmnt)
+		err := e.validate()
+		g = append(g, e)
 	}
 	return &g, nil
 }
 
 func (g *Grupin) String() string {
-	// TODO: use encoder interface
-	data, err := yaml.Marshal(g)
-	if err != nil {
-		panic("grups could not be marshalled")
+	w strings.Builder()
+	enc := yaml.NewEncoder(w)
+	for e := range g {
+		err := enc.Encode(e)
+		if err != nil {
+			panic("gruping could not be encoded")
+		}
 	}
-	return string(data)
+	return w.String()
 }
