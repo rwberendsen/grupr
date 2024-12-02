dcfeb1f53f86e242614d05675ed780d504d3065e
Richard Berendsen <guust@richardberendsen.nl>
2024-12-02 16:40:05 +0100

    Some more tests

---
+++ internal/semantics/obj_expr_test.go
@@ -0,0 +1,78 @@
+package semantics
+
+import (
+	"testing"
+)
+
+func newObjExprOrPanic(s string) ObjExpr {
+	if o, err := newObjExpr(s); err == nil {
+		return o
+	}
+	panic("error instantiating ObjExpr")
+}
+
+func TestObjExprSubsetOf(t * testing.T) {
+	tests := []struct{
+		lhs ObjExpr
+		rhs ObjExpr
+		want bool
+	}{
+	// TODO implement tests
+	// *.*.*	whatever	!disjoint
+	// a.*.*	b.*.*		disjoint
+	// a.*.c	a.b.c		!disjoint
+	// a.*.c	a.b.d		disjoint
+	// ...
+		{
+			lhs: newObjExprOrPanic("*.*.*"),
+			rhs: newObjExprOrPanic("a.b.c"),
+			want: false,
+		},
+		{
+			lhs: newObjExprOrPanic("a.b.c"),
+			rhs: newObjExprOrPanic("*.*.*"),
+			want: true,
+		},
+		{
+			lhs: newObjExprOrPanic("*.*.c"),
+			rhs: newObjExprOrPanic("*.*.*"),
+			want: true,
+		},
+		{
+			lhs: newObjExprOrPanic("a.*.*"),
+			rhs: newObjExprOrPanic("*.*.*"),
+			want: true,
+		},
+		{
+			lhs: newObjExprOrPanic("a.*.*"),
+			rhs: newObjExprOrPanic("*.b.*"),
+			want: false,
+		},
+		{
+			lhs: newObjExprOrPanic("a.b.*"),
+			rhs: newObjExprOrPanic("*.b.*"),
+			want: true,
+		},
+		{
+			lhs: newObjExprOrPanic("a.b.c"),
+			rhs: newObjExprOrPanic("*.b.*"),
+			want: true,
+		},
+		{
+			lhs: newObjExprOrPanic("a.b.c"),
+			rhs: newObjExprOrPanic("*.b.c"),
+			want: true,
+		},
+		{
+			lhs: newObjExprOrPanic("a.a.c"),
+			rhs: newObjExprOrPanic("*.b.c"),
+			want: false,
+		},
+	}
+	for _, test := range tests {
+		if test.want != test.lhs.subsetOf(test.rhs) {
+			t.Errorf("\"%v.\".subsetOf(\"%v\") not equal to %v", test.lhs, test.rhs, test.want)
+		}
+	}
+}
+
