5054f1cfd37f40697edc19d358868e1ebd35c97b
Richard Berendsen <guust@richardberendsen.nl>
2024-12-03 10:05:32 +0100

    Thought process for S3 as a backend

--- cmd/grupr/grupr.go
+++ cmd/grupr/grupr.go
@@ -18,6 +18,23 @@ func main() {
 	}
 	fmt.Printf("args: %v\n", flag.Args())
 
+	// TODO: while deserializing into Grupin, also gunzip, and
+	// calculate hash based on gzipped bytes. (using something like, io.TeeReader)
+
+	// at the same time, download the existing gzipped file from S3, if any, and
+	// compute it's running hash. Also capture the Etag.
+
+	// Now, if the hash is the same, we can just stop (all good, nothing to change)
+	// If the hash is different though, then we should do an S3 upload.
+        // Because this script may run on distributed compute, it should be idempotent.
+	// We will use a conditional write, and only overwrite if the Etag of the object
+	// has not changed since we downloadded the file.
+
+	// Since we can only decide to write until after we've read in the whole file,
+        // we'll need to keep the whole file in memory; unless we could write it to a
+	// temp key in S3 and then copy them; S3 CopyObject does support condtional write
+	// headers; most likely they would be applied on the target object for the copy
+	// operation. So, yeah, most likely this would work.
 	newGrupin, err := util.GetGrupinFromPath(flag.Arg(0))
 	if err != nil {
 		log.Fatalf("get new grupin: %v", err)
--- internal/util/io.go
+++ internal/util/io.go
@@ -20,22 +20,4 @@ func GetGrupinFromPath(path string) (semantics.Grupin, error) {
 	}
 	r, err = semantics.NewGrupin(g)
 	return r, err
-
-//	tmpl, err := template.New("grups").Funcs(template.FuncMap{"getEnv": getEnv}).Parse(string(data))
-//	if err != nil {
-//		return r, fmt.Errorf("parsing template: %s", err)
-//	}
-//	var rendered bytes.Buffer
-//	if err := tmpl.Execute(&rendered, nil); err != nil {
-//		return r, fmt.Errorf("rendering template: %s", err)
-//	}
-//	grups, err := syntax.NewGrups(rendered.Bytes())
-//	if err != nil {
-//		return r, fmt.Errorf("getting grups: %s", err)
-//	}
-//	r, err = semantics.NewGrups(grups)
-//	if err != nil {
-//		return r, fmt.Errorf("semantic error: %s", err)
-//	}
-//	return r, nil
 }
