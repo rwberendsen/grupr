34cf75ceb3b8f31bb5ead59c91cfe19ba48b9047
Richard Berendsen <guust@richardberendsen.nl>
2025-12-05 15:10:37 +0100

    WIP retry logic

--- internal/snowflake/matched.go
+++ internal/snowflake/matched.go
@@ -18,7 +18,7 @@ func newMatched(oms semantics.ObjMatchers) Matched {
 }
 
 func (m *Matched) refresh(ctx context.Context, conn *sql.DB, oms semantics.ObjMatchers, c *accountCache) error {
-	for k, v := range oms {
+	for k, v := range m {
 		if err := c.match(ctx, conn, k, v); err != nil { return err }
 	}
 	for k := range m.Exclude {
--- internal/snowflake/product.go
+++ internal/snowflake/product.go
@@ -9,51 +9,55 @@ import (
 
 type Product struct {
 	refreshCount int
-	Matched    Matched
-	Interfaces map[string]Interface
+	AccountObjects map[semantics.ObjExpr]*AccountObjs
 }
 
 func newProduct(pSem semantics.Product) *Product {
 	p := &Product{}
-	p.Matched = newMatched(pSem.ObjectMatchers)
-	p.Interfaces = map[string]Interface{}
-	for k, v := range pSem.Interfaces {
-		p.Interfaces[k] = newInterface(v)
+	p.AccountObjects = map[semantics.ObjExpr]*AccountObjs{}
+	for k := range pSem.ObjectMatchers {
+		p.AccountObjects[k] = &AccountObjects{}
 	}
 }
 
-func (pSnow *Product) refresh(ctx context.Context, cnf *Config, conn *sql.DB,  pSem semantics.Product, c *accountCache) error {
+func (p *Product) refresh(ctx context.Context, cnf *Config, conn *sql.DB, c *accountCache) error {
 	defer refreshCount += 1
 	if p.refreshCount == cnf.MaxProductRefreshCount {
 		return fmt.Errorf("Max product refresh count reached")
 	}
 	util.SleepContext(ctx, 1 << p.refreshCount - 1) // exponential backoff
-	
-	for err := p.Matched.refresh(ctx, conn, pSem.ObjectMatcher, c); err != nil {
-	}
-	if err != nil {
-		if err == ErrObjectNotExistOrAuthorized {
-			// During work objects may have been dropped; retry
-			err := pSnow.refresh(ctx, cnf, conn, pSem, c)
+	for err := p.refreshObjExprs(ctx, conn, c); err != nil {
+		if err != ErrObjectNotExistOrAuthorized {
+			return err
 		}
-		return err // the result of returning a non nil error will be that all product refreshes are cancelled by the errgroup.Group
+		err = p.refresh(ctx, cnf, conn, c) 
 	}
-	// Okay, so reaching out to the database went well, the rest is just a matter of some in-memory data structure walking,
-	// we can start to update pSnow
-	pSnow.Matched = matched
-	pSNow.Interfaces = map[string]Interface{} // initialize / reset
-	for k, v := range pSem.Interfaces {
-		pSnow.Interfaces[k] = newInterface(v, pSnow.Matched)
-	}
-	// TODO: query grants to (database) roles associated with product; do this only once, we don't expect it to be fluid, as 
-	// grupr should be the only utiilty manipulating the privileges in gruprs scope on roles grupr manages. So, if already
-	// initialized, then no need to query grants again on subsequent invocations of refreshProduct
 	return nil
+}
+
+func (p *Product) refreshObjExprs(ctx context.Context, conn *sql.DB, c *accountCache) error {
+	for k, v := range p.AccountObjects {
+		c.match() // WIP
+	}
 }
 
 func (pSnow *Product) grant(ctx context.Context, cnf *Config, conn *sql.DB, pSem semantics.Product, c *accountCache) error {
 	// if during granting we get ErrObjectNotExistOrAuthorized, we should refresh the product and try again
+
+	// only during doing something like granting will we compute stuff like objects in Interfaces, and what should
+	// be excluded.
+	// WIP ...
+	interfaces = map[string]Interface{} // initialize / reset
+	for k, v := range pSem.Interfaces {
+		pSnow.Interfaces[k] = newInterface(v, pSnow.Matched)
+	}
 }
 
 func (pSnow *Product) revoke(ctx context.Context, cnf *Config, conn *sql.DB, pSem semantics.Product, c *accountCache) error {
+	// if during granting we get ErrObjectNotExistOrAuthorized, we should refresh the product and then first grant
+	// again, and then revoke
+}
+
+func (pSnow *Product) tableCount() int {
+	// todo for basic stats, that one also needs some rewriting
 }
